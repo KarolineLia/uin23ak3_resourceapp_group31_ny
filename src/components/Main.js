eeb99cb1bf4800a2af0251eae626a4e2a1fdd00e
KarolineLia <karoliml@hiof.no>
2023-03-09 12:10:50 +0100

    la til noe på main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,10 @@
+import React from 'react';
+
 export default function Main(){
     return(
-        <>
-        <h2>Velkommen til Ressursarkivet</h2>
-        <p>Bruk menyen til å finne frem til en rekke ressurser knyttet til UiN23</p>
-        </>
+        <div id="welcomePage">
+            <h2>Velkommen til Ressursarkivet</h2>
+            <p>Bruk menyen til å finne frem til en rekke ressurser knyttet til UiN23</p>
+        </div>
     )
 }
